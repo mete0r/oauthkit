c48d75cec43179795ba922e151e0504e47c7b30b
mete0r <mete0r@sarangbang.or.kr>
2013-08-24 18:43:22 +0900

    panel: enhance ui a bit

--- data/panel.js
+++ data/panel.js
@@ -12,15 +12,13 @@ addon.port.on('token', function(token) {
 
 
 function TokenControl($scope) {
-  window.console.log('TokenControl');
+  window.console.log('TokenControl: ' + addon.options.name);
   $scope.title = addon.options.name;
   $scope.token = null;
   $scope.authenticate = function() {
-    window.console.log('authenticate clicked');
     addon.port.emit('authenticate');
   };
   $scope.refresh = function() {
-    window.console.log('refresh clicked');
     addon.port.emit('refresh');
   };
 }
